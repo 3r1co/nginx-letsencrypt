5218905c10a6c9f00a96fae5201ec14bfdd8846f
Eric Muellenbach <emuellenbach@gmail.com>
2017-02-05 17:44:21 +0100

    revert to certbot

--- letsencrypt/certbot.go
+++ letsencrypt/certbot.go
@@ -24,16 +24,14 @@ func requestCertificates(cfg Config, hosts []string) {
 	fmt.Printf("Using Certbot to generate new certificates for %v \n", hosts)
 
 	//certonly --webroot -w /var/www/example/ --agree-tos -n -m a@b.com --expand -d a.domain.co
-	//cmd := "certbot"
-	cmd := "echo"
+	cmd := "certbot"
 	emailArg := fmt.Sprintf("-m %s", cfg.Email)
 	wwwArg := fmt.Sprintf("-w %s", cfg.WwwRoot)
-	args := []string{"-n", "\"", "certonly", "--webroot", "--agree-tos", "--non-interactive", "--expand", wwwArg, emailArg}
+	args := []string{"certonly", "--webroot", "--agree-tos", "--non-interactive", "--expand", wwwArg, emailArg}
 
 	for _, host := range hosts {
 		args = append(args, fmt.Sprintf("-d %s", host))
 	}
-	args = append(args, "\"")
 
 	if out, err := exec.Command(cmd, args...).Output(); err != nil {
 		fmt.Println(err)
